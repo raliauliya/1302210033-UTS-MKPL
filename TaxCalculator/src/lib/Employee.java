2cf787f62cce166e356648422cd9c81f4f979542
raliauliya <121374492+raliauliya@users.noreply.github.com>
2024-04-23 17:17:27 +0700

    Menambahkan enumerasi Gender
    Menambahkan enum Gender pada Employee.java

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -19,8 +19,13 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
+	private Gender gender; //true = Laki-laki, false = Perempuan
+	// Primitive Obsession, menambahkan enumerasi Gender
+	enum Gender {
+		MALE,
+		FEMALE
+	}
+
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
@@ -50,25 +55,25 @@ public class Employee {
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
-	/**
-	 * Long Method 
-	 */
-	public void setMonthlySalary(int grade) {
-        switch (grade) {
-            case 1:
-                monthlySalary = 3000000;
-                break;
-            case 2:
-                monthlySalary = 5000000;
-                break;
-            case 3:
-                monthlySalary = 7000000;
-                break;
-        }
-        if (isForeigner) {
-            monthlySalary *= 1.5;
-        }
-    }
+	
+	public void setMonthlySalary(int grade) {	
+		if (grade == 1) {
+			monthlySalary = 3000000;
+			if (isForeigner) {
+				monthlySalary = (int) (3000000 * 1.5);
+			}
+		}else if (grade == 2) {
+			monthlySalary = 5000000;
+			if (isForeigner) {
+				monthlySalary = (int) (3000000 * 1.5);
+			}
+		}else if (grade == 3) {
+			monthlySalary = 7000000;
+			if (isForeigner) {
+				monthlySalary = (int) (3000000 * 1.5);
+			}
+		}
+	}
 	
 	public void setAnnualDeductible(int deductible) {	
 		this.annualDeductible = deductible;
